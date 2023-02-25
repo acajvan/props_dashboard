27f13087699fa5ae071a79e13a5bf68bf8d58303
jumongon <swiftstyles23@gmail.com>
2023-02-25 20:56:22 +0200

    just a few adjustments

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -42,7 +42,7 @@ function App() {
       //save user to mongoDB
 
       if(profileObj) {
-        const response = await fetch('http://localhost:8080/api/v1/users', {
+        const response = await fetch('https://sinologyprops.onrender.com/api/v1/users', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
@@ -112,7 +112,7 @@ function App() {
       <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
       <RefineSnackbarProvider>
         <Refine
-          dataProvider={dataProvider("http://localhost:8080/api/v1")}
+          dataProvider={dataProvider("https://sinologyprops.onrender.com/api/v1")}
           notificationProvider={notificationProvider}
           ReadyPage={ReadyPage}
           catchAll={<ErrorComponent />}
